a3e5680ead4f892dc192b82592198009b1fdaf8c
code325 <47559770+code325@users.noreply.github.com>
2019-03-24 12:54:32 +0900

    Create Hello World.cob

---
+++ Hello World.cob
@@ -0,0 +1,6 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HelloWorld.
+      *AUTHOR Manuel Coppotelli (manuelcoppotelli)
+       PROCEDURE DIVISION.
+           DISPLAY "안녕 세상아".
+       STOP RUN.
