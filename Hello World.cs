313d531c1a156663f5dd365c2b817e64af2afc70
ttakkku <k2005118@gmail.com>
2019-03-24 11:00:46 +0900

    Updata csharp

---
+++ Hello World.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello_World
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+           console.WriteLine("Hello World");
+           console.ReadLine();
+         }
+    }
+}
